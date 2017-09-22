bd2aef4ce983db76eb3813ee527547345e80202f
brendykennion <31749432+brendykennion@users.noreply.github.com>
2017-09-21 21:13:16 -0500

    ejemplo de java

---
+++ pc2j_clase3/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Bienvenidos al mundo de java!");
+       // tipos de enteros
+        byte d; // 8 bits
+        short b; // 16 bits
+        int a=0; // 32 bits
+        long d; // 64 bits
+        //tipos de datos decimales
+        float e= 0.0; // 32 bits
+        double f= 0.0; // 64 bits
+         // caracteres
+            char g = 'a';
+            //cadenas
+        String h="a";
+        // booleanos
+        boolean i=true;
+        i=false;
+
+
+
+    }
+}
